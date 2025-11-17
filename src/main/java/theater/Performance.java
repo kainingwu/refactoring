8862e4e1766c7eb7b0abfdfa3c079a704602fd71
kainingwu <kainingwu.gz@gmail.com>
2025-11-16 19:49:43 -0500

    Task1.1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -5,11 +5,27 @@ package theater;
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playID;
+    private int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    /**
+     * Returns the playID of the performance.
+     * @return the playID
+     */
+    public String getPlayID() {
+        return playID;
+    }
+
+    /**
+     * Returns the audience of the performance.
+     * @return the audience
+     */
+    public int getAudience() {
+        return audience;
     }
 }
--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,31 @@
 package theater;
 
+/**
+ * The class representing a play.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    /**
+     * Returns the name of the play.
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the type of the play.
+     * @return the type
+     */
+    public String getType() {
+        return type;
     }
 }
